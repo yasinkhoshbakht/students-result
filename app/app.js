3f16f6108c94311678ccac65e7b67001ba04a50e
yasin_wolf <yasinkhoshabkht1400@gmail.com>
2024-02-29 22:04:40 +0330

    finished

---
+++ app/app.js
@@ -0,0 +1,35 @@
+let yasinKhoshbakht = {
+  name: "yasinKhoshbakht",
+  classActivity: "A-",
+  handOut: "A+",
+  exam: "A+",
+  overal: "A+",
+};
+let rohamGhasemi = {
+  name: "rohamGhasemi",
+  classActivity: "A+",
+  handOut: "A+",
+  exam: "A+",
+  overal: "A+",
+};
+let aboulfazlEbrahimi = {
+  name: "aboulfazlEbrahimi",
+  classActivity: "C+",
+  handOut: "C+",
+  exam: "B-",
+  overal: "B-",
+};
+let prhamSobhani = {
+  name: "prhamSobhani",
+  classActivity: "C+",
+  handOut: "C+",
+  exam: "C+",
+  overal: "C+",
+};
+let listofStudent = [
+  yasinKhoshbakht,
+  rohamGhasemi,
+  aboulfazlEbrahimi,
+  prhamSobhani,
+];
+console.log(listofStudent[0]);
